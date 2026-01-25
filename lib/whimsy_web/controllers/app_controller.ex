8612c46506a0bcf31b0ecb46444a6fa31c020380
Kristoff Finley <kristoffgearhartfinley@gmail.com>
2026-01-25 15:34:13 -0800

    add vars

--- lib/whimsy_web/controllers/app_controller.ex
+++ lib/whimsy_web/controllers/app_controller.ex
@@ -1,6 +1,9 @@
 defmodule WhimsyWeb.AppController do
   use WhimsyWeb, :controller
 
+  @temperature 1.0
+  @max_tokens 5000
+
   def index(conn, _params) do
     conn
     |> render(:index)
@@ -13,8 +16,8 @@ defmodule WhimsyWeb.AppController do
       ReqLLM.generate_text!(
         model,
         ~s(Please generate a succinct description of a dungion room matching the user's descirption:\n<user_description>\n#{description}\n</user_description>),
-        temperature: 1.0,
-        max_tokens: 5000
+        temperature: @temperature,
+        max_tokens: @max_tokens
       )
 
     conn
