ef66fabd1c18df660b90865efbff419b04c65352
Kristoff Finley <kristoffgearhartfinley@gmail.com>
2026-01-25 15:31:30 -0800

    Query off to req_llm

--- lib/whimsy_web/controllers/app_controller.ex
+++ lib/whimsy_web/controllers/app_controller.ex
@@ -6,12 +6,20 @@ defmodule WhimsyWeb.AppController do
     |> render(:index)
   end
 
-  def generate_encounter(conn, _params) do
-    Process.sleep(2000)
+  def generate_encounter(conn, %{"description" => description}) do
+    model = "openai:gpt-4o-mini"
+
+    response =
+      ReqLLM.generate_text!(
+        model,
+        ~s(Please generate a succinct description of a dungion room matching the user's descirption:\n<user_description>\n#{description}\n</user_description>),
+        temperature: 1.0,
+        max_tokens: 5000
+      )
 
     conn
     |> put_root_layout(false)
     |> put_layout(false)
-    |> render(:encounter)
+    |> render(:encounter, response: response)
   end
 end
